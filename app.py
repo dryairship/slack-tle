4c6beeece67226251cfb2c963017f34dbe3f364c
Priydarshi Singh <dryairship@gmail.com>
2020-04-23 02:06:34 +0530

    Respond to bot mentions

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
 import logging
+import ssl
+import certifi
 from flask import Flask
 from slack import WebClient
 from slackeventsapi import SlackEventAdapter
-import ssl as ssl_lib
-import certifi
-
+from bot_messages import handle_command, handle_invalid_channel
 
 app = Flask(__name__)
 slack_events_adapter = SlackEventAdapter(os.environ['SLACK_SIGNING_SECRET'], "/slack/events", app)
@@ -14,28 +14,31 @@ bot_user_id = None
 bot_id = None
 bot_username = None
 
-@slack_events_adapter.on("message")
-def message(payload):
+def channel_is_valid(channel_id):
+    info = slack_web_client.conversations_info(channel=channel_id)
+    return 'is_private' in info['channel'] and not info['channel']['is_private']
+
+@slack_events_adapter.on("app_mention")
+def app_mention(payload):
     event = payload.get("event", {})
 
     channel_id = event.get("channel")
     user_id = event.get("user")
     text = event.get("text")
 
-    if user_id == bot_user_id:
-        return
+    message = handle_invalid_channel()
+    if channel_is_valid(channel_id):
+        message = handle_command(user_id, text)
 
-    message = "Hey! As of now, I only understand meows. So if you meow to me I'll meow back."
-    if text and text.lower() == "meow":
-        message = "Meeoowwwww!!!"
     response = slack_web_client.chat_postMessage(channel=channel_id, text=message)
+
 
 if __name__ == "__main__":
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
     logger.addHandler(logging.StreamHandler())
 
-    ssl_context = ssl_lib.create_default_context(cafile=certifi.where())
+    ssl_context = ssl.create_default_context(cafile=certifi.where())
 
     client_data = slack_web_client.auth_test()
     bot_user_id = client_data['user_id']
---
+++ bot_messages.py
@@ -0,0 +1,5 @@
+def handle_invalid_channel():
+    return "The bot is currently disabled in private channels and DMs."
+
+def handle_command(user_id, text):
+    return f"Hello {user_id}!"
