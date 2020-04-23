8740c340d6a81217c6ad8b1cc4ba197f11adfb25
Priydarshi Singh <dryairship@gmail.com>
2020-04-24 01:27:12 +0530

    Add graph plotting utils

---
+++ util/graphs/graph_common.py
@@ -0,0 +1,35 @@
+import os
+import io
+import time
+
+import constants
+from matplotlib import pyplot as plt
+
+
+# String wrapper to avoid the underscore behavior in legends
+#
+# In legends, matplotlib ignores labels that begin with _
+# https://matplotlib.org/api/pyplot_api.html#matplotlib.pyplot.legend
+# However, this check is only done for actual string objects.
+class StrWrap:
+    def __init__(self, s):
+        self.string = s
+    def __str__(self):
+        return self.string
+
+def get_current_figure_as_file():
+    filename = f'tempplot_{time.time()}.png'
+    filepath = os.path.join(constants.TEMP_DIR, filename)
+    plt.savefig(filepath, facecolor=plt.gca().get_facecolor(), bbox_inches='tight', pad_inches=0.25)
+    return f'{constants.HOST_NAME}/data/{filename}'
+
+def plot_rating_bg(ranks):
+    ymin, ymax = plt.gca().get_ylim()
+    bgcolor = plt.gca().get_facecolor()
+    for rank in ranks:
+        plt.axhspan(rank.low, rank.high, facecolor=rank.color_graph, alpha=0.8, edgecolor=bgcolor, linewidth=0.5)
+
+    locs, labels = plt.xticks()
+    for loc in locs:
+        plt.axvline(loc, color=bgcolor, linewidth=0.5)
+    plt.ylim(ymin, ymax)
