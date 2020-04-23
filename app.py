d0ad8b4db58f1b88af02501f94b572ffbc40b763
Priydarshi Singh <dryairship@gmail.com>
2020-04-23 19:05:13 +0530

    [WIP] Add user fetch and rating commands

--- app.py
+++ app.py
@@ -2,17 +2,24 @@ import os
 import logging
 import ssl
 import certifi
+import time
 from flask import Flask
 from slack import WebClient
 from slackeventsapi import SlackEventAdapter
-from bot_messages import handle_command, handle_invalid_channel
+from bot_messages import BotMessagesManager
+import asyncio
+import util.codeforces_api as cf
 
 app = Flask(__name__)
 slack_events_adapter = SlackEventAdapter(os.environ['SLACK_SIGNING_SECRET'], "/slack/events", app)
 slack_web_client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
+
 bot_user_id = None
 bot_id = None
 bot_username = None
+bot_message_manager = None
+
+event_loop = None
 
 def channel_is_valid(channel_id):
     info = slack_web_client.conversations_info(channel=channel_id)
@@ -21,14 +28,19 @@ def channel_is_valid(channel_id):
 @slack_events_adapter.on("app_mention")
 def app_mention(payload):
     event = payload.get("event", {})
-
     channel_id = event.get("channel")
     user_id = event.get("user")
     text = event.get("text")
 
-    message = handle_invalid_channel()
+    event_ts = float(event.get("ts"))
+    current_ts = time.time()
+    time_difference = current_ts-event_ts
+    if time_difference > 5:
+        return
+
+    message = bot_message_manager.handle_invalid_channel()
     if channel_is_valid(channel_id):
-        message = handle_command(user_id, text)
+        message = bot_message_manager.handle_command(bot_user_id, user_id, text)
 
     response = slack_web_client.chat_postMessage(channel=channel_id, text=message)
 
@@ -44,5 +56,10 @@ if __name__ == "__main__":
     bot_user_id = client_data['user_id']
     bot_id = client_data['bot_id']
     bot_username = client_data['user']
+
+    event_loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(event_loop)
+    event_loop.run_until_complete(cf.initialize())
+    bot_message_manager = BotMessagesManager(event_loop)
 
     app.run(port=3000)
--- bot_messages.py
+++ bot_messages.py
@@ -1,5 +1,41 @@
-def handle_invalid_channel():
-    return "The bot is currently disabled in private channels and DMs."
+import util.codeforces_api as cf
+import json
+import asyncio
 
-def handle_command(user_id, text):
-    return f"Hello {user_id}!"
+class BotMessagesManager:
+    def __init__(self, event_loop):
+        self.event_loop = event_loop
+
+    def handle_invalid_channel(self):
+        return "The bot is currently disabled in private channels and DMs."
+
+    def handle_help(self):
+        return '''
+        Hi! The following commands are supported as of now:
+        `help`
+        `fetch`
+        `rating`
+        '''
+
+    def handle_fetch_user(self, user):
+        result = self.event_loop.run_until_complete(cf.user.info(handles=[user]))
+        return json.dumps(result)
+
+    def handle_rating_user(self, user):
+        result = self.event_loop.run_until_complete(cf.user.rating(handle=user))
+        return json.dumps(result)
+
+    def handle_command(self, bot_user_id, user_id, text):
+        parts = text.split()
+
+        if not bot_user_id in parts[0]:
+            return "I'm a busy bot. Call me only when you need me. :angry:"
+
+        if parts[1] == "help":
+            return self.handle_help()
+        if parts[1] == "fetch":
+            return self.handle_fetch_user(parts[2])
+        if parts[1] == "rating":
+            return self.handle_rating_user(parts[2])
+
+        return self.handle_help()
